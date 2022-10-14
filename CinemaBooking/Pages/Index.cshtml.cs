aa24095e5bf1a9a15500bb82c00dde38b5139aad
David Alvarez <davidj398@yahoo.com>
2022-10-14 14:47:23 -0400

    Made some styling/theme changes

--- CinemaBooking/Pages/Index.cshtml.cs
+++ CinemaBooking/Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace CinemaBooking.Pages
 {
